02f85a2bf42309a3b1bc99b500f212aa86451232
hafidzamr <hafidzamrulloh0797@gmail.com>
2019-09-07 16:12:58 +0700

    Updates

---
+++ precache-manifest.4b58b575dd3d229cb827e1e234f2a53c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ffec62ec7528d4fb0a07b6a2f448ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09aaf71d93492ebb6d83",
+    "url": "/static/css/main.54a7c929.chunk.css"
+  },
+  {
+    "revision": "caa9a7fa08bf2c0d73b8",
+    "url": "/static/js/2.f0910273.chunk.js"
+  },
+  {
+    "revision": "09aaf71d93492ebb6d83",
+    "url": "/static/js/main.35572c3a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "446f29a79f52721b8cda008a5c2920d3",
+    "url": "/static/media/bg-img.446f29a7.png"
+  },
+  {
+    "revision": "46a4bd8fa4fe44b3d1e657f5c80c2feb",
+    "url": "/static/media/result-img.46a4bd8f.png"
+  }
+]);
